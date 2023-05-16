b7bb902fc930db50bb0631de217f4df4b5cf17ed
sheroques <luizalexandre99@hotmail.com.br>
2023-05-16 12:20:22 -0300

    primeira aula NLW

---
+++ Aula 1/web/src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from 'next/image'
+
+
+export default function Home() {
+  return (
+   <div className="h-screen bg-zinc-950 text-zinc-50 p-6">
+    <h1 className="text-4xl font-bold">Sua capsula do tempo</h1>
+   </div>
+  )
+}
